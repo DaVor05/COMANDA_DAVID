5b7046e80645c2b68f34c87d72cbd2efbaac4386
Brandon666oscuro <brandon.rodriguezvillanueva@cesunbc.edu.mx>
2025-12-05 23:40:03 -0800

    Descripción de los cambios

--- core_main_test/settings.py
+++ core_main_test/settings.py
@@ -20,6 +20,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -119,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/img/'
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
